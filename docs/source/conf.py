e565116ee17fe7bdf228dd2323f544dd05a706e7
Angel Woo <awoo424@gmail.com>
2020-06-25 19:23:16 +0900

    Test with custom css

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -52,8 +52,11 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-# html_theme = "sphinx_rtd_theme"
-html_style = 'custom.css'
+html_theme = "sphinx_rtd_theme"
+# html_style = 'custom.css'
+html_context = {
+    'css_files': ['_static/custom.css'],
+}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
