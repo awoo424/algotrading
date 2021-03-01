c64c598a8ca0baea5e58567e44849d24b6e047c6
Angel Woo <awoo424@gmail.com>
2021-03-02 02:32:00 +0800

    Update inline comments

--- code/integrated-strategy/macro_analysis.py
+++ code/integrated-strategy/macro_analysis.py
@@ -10,19 +10,20 @@ mpl.use('tkagg')  # issues with Big Sur
 # input: @df, stock tick df
 # output: s_gdp, s_unemploy, s_property
 def GetSensitivity(price_df):
+    # Stock price dataframe pre-processing
     price_df = price_df.reset_index(level='Date')
     price_dates = price_df['Date'].tolist()
     #print(price_df.head())
 
     # load GDP data
     gdp_df = pd.read_csv('../../database_real/macroeconomic_data/determinants/quarterly_gdp.csv', header=0)
+    
     gdp_df['Date'] = pd.to_datetime(gdp_df['Date'])
-    gdp_dates = gdp_df['Date'].tolist()
-
     gdp_dates = gdp_df['Date'].tolist()
     gdp_values = gdp_df['GDP_current_market_prices'].tolist()
     gdp_counter = 0
 
+    # merge dataframes by adding GDP column in price_df
     for date in price_dates:
         target_date = gdp_dates[gdp_counter]
         if (target_date <= date) and (gdp_counter < len(gdp_values)):
@@ -31,15 +32,15 @@ def GetSensitivity(price_df):
         elif (gdp_counter < (len(gdp_values) - 1)):
             gdp_counter = gdp_counter + 1
 
-    # column pre-processing
+    # GDP column pre-processing
     price_df['GDP'] = price_df['GDP'].str.replace(',', '')
     price_df['GDP'] = price_df['GDP'].astype(float)
     price_df = price_df.fillna(value={'GDP':0})
     
+    # Calculate price-GDP correlation
     price = price_df['Close']
     gdp = price_df['GDP']
     price_gdp_corr = price.corr(gdp)
-    #print(price_gdp_corr)
 
     # load unemployment data
     unemploy_df = pd.read_csv('../../database_real/macroeconomic_data/determinants/unemployment_rate.csv', header=0)
