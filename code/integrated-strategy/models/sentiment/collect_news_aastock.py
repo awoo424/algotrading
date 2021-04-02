d5e3763f60bf1cf2a22ee7ac47197ca5989dd473
wuxuegithub <wuxue@connect.hku.hk>
2021-04-02 13:34:12 +0800

    added sentiment model

---
+++ code/integrated-strategy/models/sentiment/collect_news_aastock.py
@@ -0,0 +1,79 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
+import datetime, time
+from bs4 import BeautifulSoup
+from urllib.request import Request
+from urllib.request import urlopen
+import os
+import csv
+import pandas as pd
+dir_name= os.getcwd()+'/database_daily/'
+def get_news_aastock(ticker,postfix_url,newstype,days):
+        # initialize chrome setting from collecting stock using chrome driver
+    chrome_options = Options()  
+    chrome_options.add_argument("--headless")  
+    chrome_options.add_argument('no-sandbox')  
+    chrome_options.add_argument('-disable-dev-shm-usage')  
+    driver = webdriver.Chrome(ChromeDriverManager().install(),options=chrome_options)
+    driver.implicitly_wait(20)
+
+    prefix_url='http://www.aastocks.com/en/stocks/analysis/stock-aafn/'
+    try:
+        SCROLL_PAUSE_TIME = 2
+        fill_ticker=ticker.zfill(5)
+        url=prefix_url+fill_ticker+postfix_url
+        driver.get(url)
+        last_height = driver.execute_script("return document.body.scrollHeight")
+        
+        while True:
+            # Scroll down to bottom
+            driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+
+            # Wait to load page
+            time.sleep(SCROLL_PAUSE_TIME)
+
+            # Calculate new scroll height and compare with last scroll height
+            new_height = driver.execute_script("return document.body.scrollHeight")
+            if new_height == last_height:
+                break
+            last_height = new_height
+#         req = Request(url=url,headers={'user-agent': 'my-app/0.0.1'}) 
+#         resp = urlopen(req) 
+     
+        html=BeautifulSoup(driver.page_source, 'lxml')
+      
+        dates=html.findAll("div", {"class": "newstime4"})
+        
+        news=html.findAll("div", {"class": "newshead4"})
+        
+        idx=0
+        path=os.path.join(dir_name,'data-news/data-aastock/'+'data-'+ticker.zfill(4)+'-aastock.csv')
+        if (len(dates)>0):
+            with open(path,'a') as f:
+                writer = csv.writer(f)
+                for i in dates:
+                    
+                    if "/" in str(i.get_text()):
+                        date=str(i.get_text())
+                        if "Release Time" in date:
+                            date=date[13:23]
+                        elif (date[0]==" "):
+                            date=str(date[1:11])
+                        else:
+                            date=str(date[0:10])
+                        # print(date)
+                        text=news[idx].get_text()
+
+                        date_time_obj = datetime.datetime.strptime(date, '%Y/%m/%d')
+                        date_time=date_time_obj.strftime('%Y-%m-%d')
+                        if(datetime.datetime.now()-date_time_obj).days<=days:
+                            print(text)
+                            writer.writerow([date_time,text,ticker,newstype])
+                        idx+=1
+    except Exception as e:
+        print(e)
+        pass
+    driver.quit()
+
+    
---
+++ code/integrated-strategy/models/sentiment/sentiment_vader.py
@@ -0,0 +1,76 @@
+### Function for Vader Analysis 
+
+import pandas as pd
+import nltk
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+
+nltk.downloader.download('vader_lexicon')
+analyser = SentimentIntensityAnalyzer()
+
+
+
+# read from vader scores
+def read_news_vader_path(df):
+    print('read in datasets')
+    cs=[]
+    # append a compound score to every news row
+    for row in range(len(df)):
+        cs.append(analyser.polarity_scores(df['news'].iloc[row])['compound'])
+    # append the column to original dataset
+    df['compound_vader_score']=cs
+    return df
+
+
+# group by the mean compound vader score by dates
+def find_news_vader_pred_label(df,threshold):
+    print('find_pred_label')
+    news = df['news']
+    # group the data by dates
+    df = df.groupby(['dates'])['compound_vader_score'].mean().reset_index()
+    final_label=[]
+    
+    # convert the vader score using a threshold to a sentiment label
+    for i in range(len(df)):
+
+        if df['compound_vader_score'].iloc[i] > threshold:
+            final_label.append(2)
+        elif df['compound_vader_score'].iloc[i] < -threshold:
+            final_label.append(0)
+        elif (df['compound_vader_score'].iloc[i] >= -threshold  
+              and df['compound_vader_score'].iloc[i] <= threshold):
+            final_label.append(1)
+
+    df['vader_label'] = final_label
+    return df
+
+
+# merge the dataset with the hang seng index daily moving average
+def merge_vader_actual_label (df,hsi_movement_df):
+    print('merge_actual_label')
+    vader_data = df
+    vader_data.set_index(keys = ["dates"],inplace=True)
+    label_data = pd.read_csv(hsi_movement_df)
+    label_data.set_index(keys = ["dates"],inplace=True)
+    # inner join the two datasets using the date index
+    merge = pd.merge(vader_data,label_data, how='inner', left_index=True, right_index=True)
+    merge = merge.reset_index()
+    # drop the redudant column 
+    merge = merge.drop(['Unnamed: 0'],axis=1)
+    
+    return merge
+
+### Starter function for vader sentiment analysis ###
+def starter_vader(path,result_path):
+    # get the full path of each ticker
+   
+    df = pd.read_csv(path,names=['dates','news','ticker','newstype'])
+    # read append the compound vader score to the pandas dataframe
+    df = read_news_vader_path(df)
+    # pass in the threshold to get the vader label
+    df = find_news_vader_pred_label(df,0.01)
+    # store to the csv file if the dataset is not empty
+    df=df.drop('compound_vader_score',axis=1)
+    if (df.empty == False):
+        df.to_csv(result_path,index=False)
+    return df
+
